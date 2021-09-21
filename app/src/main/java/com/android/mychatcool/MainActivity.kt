7289c7b86c14eb24fb236e132a96a32ffd85b457
MikhailEA <michev.work@gmail.com>
2021-09-21 18:45:51 +0300

    Работа с базой, подключение кнопки взаимодействия чата

--- app/src/main/java/com/android/mychatcool/MainActivity.kt
+++ app/src/main/java/com/android/mychatcool/MainActivity.kt
@@ -2,18 +2,46 @@ package com.android.mychatcool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.mychatcool.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("message")
+        val myRef = database.getReference("messages")
 
-        myRef.setValue("Hello, Mikhail")
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
+
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Mikhail: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
 }
